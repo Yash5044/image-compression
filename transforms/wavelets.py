b1607fedce47e0d07c233d96aef68e69e5202f5b
Yash5044 <yashchaubey2001@gmail.com>
2021-01-30 02:58:34 +0530

    fixed truncate.py

--- transforms/wavelets.py
+++ transforms/wavelets.py
@@ -5,9 +5,9 @@ import pywt
 import truncate as trun
 import colormap as cmap
 import numpy as np
-path = "../images/frymire.png"
+path = "../images/baboon.png"
 wave = sys.argv[2] # eg : haar, mexh
-threshold = int(sys.argv[1])  # a number 
+threshold = float(sys.argv[1])  # a number 
 #e.g. python wavelets.py 50 haar
 
 
@@ -15,20 +15,22 @@ threshold = int(sys.argv[1])  # a number
 def wavelet(img2d):
     # loader.show_image(img2d)
     (wA, (wH, wV, wD)) = pywt.dwt2(img2d, wave) # dwt
-    cmap.graph(wA)  # to  visualize any 2d array
-    wA = trun.truncate(wA, threshold)  # approximate part
-    wH = trun.truncate(wH, threshold)  # horizontally detailed part
-    wV = trun.truncate(wV, threshold)  # vertically detailed part
-    wD = trun.truncate(wD, threshold)  # diagonally detailed part
+    cmap.graph(wV)  # to  visualize any 2d array
+    mval = max(np.max(wA),-np.min(wA))
+    wA = trun.truncate(wA, threshold, mval)  # approximate part
+    wH = trun.truncate(wH, threshold, mval)  # horizontally detailed part
+    wV = trun.truncate(wV, threshold, mval)  # vertically detailed part
+    wD = trun.truncate(wD, threshold, mval)  # diagonally detailed part
+    cmap.graph(wV)
     imgr2d = pywt.idwt2((wA, (wH, wV, wD)), wave) # idwt to reconstruct the numpy array
     h,w = imgr2d.shape
     return imgr2d[:h-1,:] 
 
 def main():
-    img = loader.load_image(path)
+    img = loader.load_image(path)[:,:,0]
     # loader.show_image(img)
-    for i in range(3):   # i=0->R ; i=1->G ; i=2->B
-        img[:,:,i] = wavelet(img[:,:,i])
+    # for i in range(3):   # i=0->R ; i=1->G ; i=2->B
+    img = wavelet(img)
     # loader.show_image(img)
 
 
